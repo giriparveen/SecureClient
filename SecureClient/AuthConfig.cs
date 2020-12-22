71470be82421c02525d82a5761f184c089ff68f1
giriparveen <giriparveen@gmail.com>
2020-12-22 14:31:13 +0530

    First commit

---
+++ SecureClient/AuthConfig.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace SecureClient
+{
+    public class AuthConfig
+    {
+        public string InstanceId { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string Authority { 
+            get 
+            {
+                return string.Format(CultureInfo.InvariantCulture, InstanceId, TenantId);
+            } 
+        }
+        public string ResourceId { get; set; }
+
+        public static AuthConfig ReadJsonFromFile(string path)
+        {
+            IConfiguration configuration;
+            string directoryName = Path.GetDirectoryName(path);
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(path);
+            configuration = builder.Build();
+            return configuration.Get<AuthConfig>();
+        }
+    }
+}
---
+++ SecureClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace SecureClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        public static async Task RunAsync()
+        {
+            AuthConfig auth = AuthConfig.ReadJsonFromFile("appsettings.json");
+            IConfidentialClientApplication app;
+            
+            app = ConfidentialClientApplicationBuilder.Create(auth.ClientId).WithClientSecret(auth.ClientSecret)
+                .WithAuthority(new Uri(auth.Authority)).Build();
+            string[] ResourceId = new string[] { auth.ResourceId };
+            AuthenticationResult result;
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceId).ExecuteAsync();
+                Console.WriteLine(result.AccessToken);
+                if (!string.IsNullOrEmpty(result.AccessToken))
+                {
+                    var httpClient = new HttpClient();
+                    var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+                    if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any(m => m.MediaType == "application/json"))
+                    {
+                        httpClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    }
+                    defaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                    HttpResponseMessage response= await httpClient.GetAsync(auth.BaseAddress);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.InnerException);
+            }
+           
+        } 
+    }
+}
